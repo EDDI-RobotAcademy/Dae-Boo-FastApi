e28b0ffb4f0b318dbdef002b6ec2af563335f686
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-08-09 15:06:18 +0900

    update: [이상원] 간단한 main.py 구성

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
