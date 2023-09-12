3737030dc9514549dea9df6547d4897b03ad9c99
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-09-12 11:38:19 +0900

    Mercenary-Socket-Server를 Mercenary-FastAPI 에 붙이기

--- app/divide_file.py
+++ app/divide_file.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('add_card_data.csv')
 
-chunk_size = 2000000
+chunk_size = 1000000
 
 for i, chunk in enumerate(range(0,len(df),chunk_size),start = 1):
     chunk_df = df.iloc[chunk:chunk+chunk_size]
